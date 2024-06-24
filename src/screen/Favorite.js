f564c39c582cf75a0604493151bc636d8342048f
Eduardo <cesconettoedu@gmail.com>
2024-06-24 15:21:05 -0400

    solve key id

--- src/screen/Favorite.js
+++ src/screen/Favorite.js
@@ -13,31 +13,38 @@ export default function Favorite(props) {
 
   return (
     <View>
-      <TouchableOpacity
-        style={StyleSheet.compose({ backgroundColor: "rgba(255,255,255,0.3)" },tw`mx-4 p-2 mb-1 flex-row mt-2 rounded-2xl`)}
-        onPress={() => {setSelectedEvent(props.info.id); navigation.navigate("Event", { ...props.info });}}
-        key={props.info.id}
-      >
-        <Image src={props.info.image} style={S.icons} />
-        <View style={tw`flex-1 flex justify-center pl-3`}>
-          <Text style={tw`font-semibold pb-3`}>{props.info.title}</Text>
-          <Text style={tw`font-semibold`}>{props.info.date}</Text>
-        </View>
-        <View style={tw`flex justify-between items-center mr-3`}>
-          <TouchableOpacity
-            style={StyleSheet.compose(
-              { backgroundColor: "rgba(255,255,255,0.3)" },
-              tw`p-2 rounded-full `
-            )}
-          >
-            {/* mudar quando receber a props do Event.js 
-                            <FontAwesome name="heart" size={20} color={favourite ? 'red' : 'gray'} />
-                            
-                            */}
-          </TouchableOpacity>
-          <GradientBtn buttonClass="py-2 px-5" />
-        </View>
-      </TouchableOpacity>
+      <Text>Favorite</Text>
     </View>
-  );
+  )
+
+//   return (
+//     <View>
+//       <TouchableOpacity
+//         style={StyleSheet.compose({ backgroundColor: "rgba(255,255,255,0.3)" },tw`mx-4 p-2 mb-1 flex-row mt-2 rounded-2xl`)}
+//         onPress={() => {setSelectedEvent(props.info.id); navigation.navigate("Event", { ...props.info });}}
+//         key={props.info.id}
+//       >
+//         <Image src={props.info.image} style={S.icons} />
+//         <View style={tw`flex-1 flex justify-center pl-3`}>
+//           <Text style={tw`font-semibold pb-3`}>{props.info.title}</Text>
+//           <Text style={tw`font-semibold`}>{props.info.date}</Text>
+//         </View>
+//         <View style={tw`flex justify-between items-center mr-3`}>
+//           <TouchableOpacity
+//             style={StyleSheet.compose(
+//               { backgroundColor: "rgba(255,255,255,0.3)" },
+//               tw`p-2 rounded-full `
+//             )}
+//           >
+//             {/* mudar quando receber a props do Event.js 
+//                             <FontAwesome name="heart" size={20} color={favourite ? 'red' : 'gray'} />
+                            
+//                             */}
+//           </TouchableOpacity>
+//           <GradientBtn buttonClass="py-2 px-5" />
+//         </View>
+//       </TouchableOpacity>
+//     </View>
+//   );
+// 
 }
